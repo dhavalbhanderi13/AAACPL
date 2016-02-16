0f6a16f803b4b312c73b7ecee6f82d1168de62da
sumedh <sumedh@gmail.com>
2016-02-16 22:44:09 +0530

    1) Changes to return department id on create 2) Get auction by department id

---
+++ src/main/java/com/aaacpl/bo/response/CreateDepartmentResponseBO.java
@@ -0,0 +1,23 @@
+package com.aaacpl.bo.response;
+
+/**
+ * Created by Hp on 16-02-2016.
+ */
+public class CreateDepartmentResponseBO {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateDepartmentResponseBO{" +
+                "id=" + id +
+                '}';
+    }
+}

137dde1019f4d0e190ed482c78730faae8669ac7
dhavalbhanderi13 <dhavalbhanderi13@gmail.com>
2016-02-18 23:28:51 +0530

    Fixed bid and lot Status

--- src/main/java/com/aaacpl/test/Testing.java
+++ src/main/java/com/aaacpl/test/Testing.java
@@ -1,19 +1,44 @@
 package com.aaacpl.test;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import com.aaacpl.api.services.AuctionService;
-import com.aaacpl.util.DateUtil;
+import com.aaacpl.bo.request.lots.BidRequestBO;
+import com.aaacpl.dao.LotsDAO;
 
 public class Testing {
 	static AuctionService as = new AuctionService();
 
+	public static String getCurrentServerTime() {
+		String timeStamp = new SimpleDateFormat("YYYY-MM-DD HH:MM:SS")
+				.format(Calendar.getInstance().getTime());
+
+		return timeStamp;
+	}
+
 	public static void main(String[] args) {
+
+		LotsDAO dao = new LotsDAO();
+		BidRequestBO bidRequestBO = new BidRequestBO(2, 1, 800L, "10.17.150",
+				getCurrentServerTime());
+
+		try {
+			// dao.insertBid(bidRequestBO);
+			System.out.println(dao.getLotStatus(1));
+		} catch (SQLException | IOException e) {
+			// TODO Auto-generated catch block
+		}
 
 		// Test for get auctions
 
-		//System.out.println(as.getAllAuctions(1));
-		
-		System.out.println(DateUtil.getTimeStampFromString("2016-02-12 00:00:00"));
+		// System.out.println(as.getAllAuctions(1));
 
+		// System.out.println(DateUtil.getTimeStampFromString("2016-02-12 00:00:00"));
+
+		// System.out.println(getCurrentServerTime());
 		// test for create Auction
 
 		/*
